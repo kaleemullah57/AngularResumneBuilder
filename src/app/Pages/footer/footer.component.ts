829ade75d797005588df97949de11a21a706fb12
kaleemullah57 <kaleemullh1234@gmail.com>
2025-01-30 05:10:44 +0500

    pages editing

--- src/app/Pages/footer/footer.component.ts
+++ src/app/Pages/footer/footer.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
@@ -8,4 +8,33 @@ import { Component } from '@angular/core';
 })
 export class FooterComponent {
   currentYear: number = new Date().getFullYear();
+
+
+
+
+  @ViewChild('animatedContainer', { static: false }) animatedContainer!: ElementRef;
+
+  animationPlayed = false;
+
+
+  ngAfterViewInit() {
+    this.checkScroll();
+  }
+
+  @HostListener('window:scroll')
+  onWindowScroll() {
+    this.checkScroll();
+  }
+
+  private checkScroll() {
+    if (this.animationPlayed || !this.animatedContainer) return;
+
+    const elementTop = this.animatedContainer.nativeElement.getBoundingClientRect().top;
+    const windowHeight = window.innerHeight;
+
+    if (elementTop < windowHeight) {
+      this.animatedContainer.nativeElement.classList.add('animate');
+      this.animationPlayed = true;
+    }
+  }
 }
